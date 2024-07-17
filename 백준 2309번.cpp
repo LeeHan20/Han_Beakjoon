aeb8e20a995899cb3a24ff4a1a2dd375144ae0ff
LeeHan20 <162099392+LeeHan20@users.noreply.github.com>
2024-07-17 16:07:01 +0900

    Update 백준 2309번.cpp

--- 백준 2309번.cpp
+++ 백준 2309번.cpp
@@ -5,37 +5,37 @@
 using namespace std;
 
 int main() {
-    vector<int> hights(9);
-    for (int i = 0; i<9; ++i) {
-        cin>>hights[i];
+    vector<int> heights(9);
+    for (int i = 0; i < 9; ++i) {
+        cin >> heights[i];
     }
     //vector<int> hights = {20,7,23,19,10,15,25,8,13};
     //vector<int> hights = {40,40,10,10,10,10,10,10,10};
+
+    int sum_of_heights = accumulate(heights.begin(), heights.end(), 0);
     
-    int sum_of_hights = accumulate(hights.begin(), hights.end(), 0);
-    
-    for (int i = 0; i<9; ++i) {
-        long int nega_one = count(hights.begin(), hights.end(), -1);
-        if (nega_one == 2){
-            break;
-        }
-        for (int j = i+1; j < 10; ++j) {
-            int checker = sum_of_hights - hights[i] - hights[j];
+    for (int i = 0; i < 9; ++i) {
+        for (int j = i + 1; j < 9; ++j) {
+            int checker = sum_of_heights - heights[i] - heights[j];
             if (checker == 100) {
-                hights[i] = -1;
-                hights[j] = -1;
-        
-                }}}
-    
+                heights[i] = -1;
+                heights[j] = -1;
+                break;
+            }}
+        if (heights[i] == -1) {
+            break;
+        }}
+
     vector<int> result;
-    for (int i : hights) {
+    for (int i : heights) {
         if (i != -1) {
             result.push_back(i);
         }}
-    
+
     sort(result.begin(), result.end());
-    for (int i : result){
-        cout<< i << endl;
+    for (int i : result) {
+        cout << i << endl;
     }
+    
     return 0;
 }
