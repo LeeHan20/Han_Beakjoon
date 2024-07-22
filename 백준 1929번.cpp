6071613c3edbe08fc4656fb683dc1a3787ef0b90
LeeHan20 <162099392+LeeHan20@users.noreply.github.com>
2024-07-22 15:15:26 +0900

    Update 백준 1929번.cpp

--- 백준 1929번.cpp
+++ 백준 1929번.cpp
@@ -6,6 +6,8 @@ using namespace std;
 int main() {
     int n, m;
     cin >> n >> m;
+    // n = 3;
+    // m = 16;
     vector<int> decimal(m + 1, 1);
     decimal[0] = decimal[1] = 0;
     for (int i = 2; i <= sqrt(m); i++) {
@@ -22,3 +24,6 @@ int main() {
     }
     return 0;
 }
+
+// endl을 쓰는 것과 "\n"을 쓰는 것은 출력이 많을 때 성능 차이를 많이 유발한다. 
+// endl이 보기에는 깔끔하지만 웬만하면 "\n"을 권장함.
